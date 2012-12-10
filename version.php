b27a7aa3e036826744d068fcdd8a8956e866dadb
Nicolas Bretin <bretin@ualberta.ca>
2012-12-10 15:33:20 -0700

    Update the logo for Moodle 2.4

--- version.php
+++ version.php
@@ -15,10 +15,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012111400;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012121000;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012062500;            // Requires this Moodle version
 $plugin->component = 'format_calendar';     // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.1 (Build: 2012111400)';
+$plugin->release = '1.0.1 (Build: 2012121000)';
 $plugin->cron = 0;                          // Period for cron to check this module (se
 
