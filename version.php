4545de61c08ce0f54710caa4710570e64d813892
Nicolas Bretin <bretin@ualberta.ca>
2014-04-02 17:15:37 -0600

    Patch  Moodle  2.6

--- version.php
+++ version.php
@@ -15,10 +15,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013040200;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014040200;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013040500;            // Requires this Moodle version
 $plugin->component = 'format_calendar';     // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.5 (Build: 2013040200)';
+$plugin->release = '1.0.5 (Build: 2014040200)';
 $plugin->cron = 0;                          // Period for cron to check this module (se
 
